efb89179efda09661fad2fc4350d533b09f36bfe
Tobbe <tobias.bjerking@gmail.com>
2019-03-21 16:14:44 +0100

    Pawns can now move 2 spaces the first time they move.

--- AlphaBeta Chess/Pawn.cpp
+++ AlphaBeta Chess/Pawn.cpp
@@ -13,6 +13,8 @@ Pawn::~Pawn()
 std::vector<Position*> Pawn::getPossibleMoves(int x, int y){
 	std::vector<Position*> possiblePositions;
 	if (color == BLACK) {
+		if(x == 1)
+			possiblePositions.push_back(new Position(x + 2, y));
 		if(x+1<8 && y-1>=0)
 			if (Window::getInstance().gb->pieces[x + 1][y - 1] != nullptr && Window::getInstance().gb->pieces[x + 1][y - 1]->color != color)
 				possiblePositions.push_back(new Position(x + 1, y - 1));
@@ -24,6 +26,8 @@ std::vector<Position*> Pawn::getPossibleMoves(int x, int y){
 				possiblePositions.push_back(new Position(x + 1, y + 1));
 	}
 	else {
+		if (x == 6)
+			possiblePositions.push_back(new Position(x - 2, y));
 		if(x-1>=0 && y-1>=0)
 			if (Window::getInstance().gb->pieces[x - 1][y - 1] != nullptr && Window::getInstance().gb->pieces[x - 1][y - 1]->color != color)
 				possiblePositions.push_back(new Position(x - 1, y - 1));
