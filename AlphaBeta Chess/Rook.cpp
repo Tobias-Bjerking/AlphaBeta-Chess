feff6cd5c11bd2392f0924939f44a6a6151e7a74
Tobbe <tobias.bjerking@gmail.com>
2019-03-21 01:34:38 +0100

    Rooks movement implemented

--- AlphaBeta Chess/Rook.cpp
+++ AlphaBeta Chess/Rook.cpp
@@ -13,20 +13,58 @@ Rook::~Rook()
 
 std::vector<Position*> Rook::getPossibleMoves(int x, int y){
 	std::vector<Position*> possiblePositions;
-	if (color == BLACK) {
+	
+	for (int i = x + 1; i < 8; i++) {
+		if (Window::getInstance().gb->pieces[i][y] == nullptr)
+			possiblePositions.push_back(new Position(i, y));
+		else if (Window::getInstance().gb->pieces[i][y]->color != color) {
+			possiblePositions.push_back(new Position(i, y));
+			break;
+		}
+		else
+			break;
+	}
+	for (int i = x - 1; i >= 0; i--) {
+		if (Window::getInstance().gb->pieces[i][y] == nullptr)
+			possiblePositions.push_back(new Position(i, y));
+		else if (Window::getInstance().gb->pieces[i][y]->color != color) {
+			possiblePositions.push_back(new Position(i, y));
+			break;
+		}
+		else
+			break;
+	}
+	for (int i = y + 1; i < 8; i++) {
+		if (Window::getInstance().gb->pieces[x][i] == nullptr)
+			possiblePositions.push_back(new Position(x, i));
+		else if (Window::getInstance().gb->pieces[x][i]->color != color) {
+			possiblePositions.push_back(new Position(x, i));
+			break;
+		}
+		else
+			break;
+	}
+	for (int i = y - 1; i >= 0; i--) {
+		if (Window::getInstance().gb->pieces[x][i] == nullptr)
+			possiblePositions.push_back(new Position(x, i));
+		else if (Window::getInstance().gb->pieces[x][i]->color != color) {
+			possiblePositions.push_back(new Position(x, i));
+			break;
+		}
+		else
+			break;
+	}
 
-		if (Window::getInstance().gb->pieces[x - 1][y + 1] != nullptr && Window::getInstance().gb->pieces[x - 1][y + 1]->color != color)
-			possiblePositions.push_back(new Position(x - 1, y + 1));
-		possiblePositions.push_back(new Position(x, y + 1));
-		if (Window::getInstance().gb->pieces[x + 1][y + 1] != nullptr && Window::getInstance().gb->pieces[x + 1][y + 1]->color != color)
-			possiblePositions.push_back(new Position(x + 1, y + 1));
-	}
-	else {
-		if (Window::getInstance().gb->pieces[x - 1][y - 1] != nullptr && Window::getInstance().gb->pieces[x - 1][y - 1]->color != color)
-			possiblePositions.push_back(new Position(x - 1, y - 1));
-		possiblePositions.push_back(new Position(x, y - 1));
-		if (Window::getInstance().gb->pieces[x + 1][y - 1] != nullptr && Window::getInstance().gb->pieces[x + 1][y - 1]->color != color)
-			possiblePositions.push_back(new Position(x + 1, y - 1));
-	}
+	for (int i = -1; i <= 1; i++)
+		for (int j = -1; j <= 1; j++)
+			if (x + i < 8 && x + i >= 0 && y + j < 8 && y + j >= 0)
+				if (Window::getInstance().gb->pieces[x + i][y + j] == nullptr)
+					possiblePositions.push_back(new Position(x + i, y + j));
+				else if (Window::getInstance().gb->pieces[x + i][y + j]->color != color) {
+					possiblePositions.push_back(new Position(x + i, y + j));
+					break;
+				}
+				else
+					break;
 	return possiblePositions;
 }
