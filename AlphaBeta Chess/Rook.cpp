bf972679be89804543afdc8ef41c2dd9e4dd966e
Tobbe <tobias.bjerking@gmail.com>
2019-03-21 16:00:02 +0100

    Removed unnecessary code from rooks movement

--- AlphaBeta Chess/Rook.cpp
+++ AlphaBeta Chess/Rook.cpp
@@ -55,16 +55,5 @@ std::vector<Position*> Rook::getPossibleMoves(int x, int y){
 			break;
 	}
 
-	for (int i = -1; i <= 1; i++)
-		for (int j = -1; j <= 1; j++)
-			if (x + i < 8 && x + i >= 0 && y + j < 8 && y + j >= 0)
-				if (Window::getInstance().gb->pieces[x + i][y + j] == nullptr)
-					possiblePositions.push_back(new Position(x + i, y + j));
-				else if (Window::getInstance().gb->pieces[x + i][y + j]->color != color) {
-					possiblePositions.push_back(new Position(x + i, y + j));
-					break;
-				}
-				else
-					break;
 	return possiblePositions;
 }
