808e12c0da09bbffecc1cd3b9414374b3ee0d58e
Tobbe <tobias.bjerking@gmail.com>
2019-03-21 17:31:06 +0100

    add copy constructor to gameboard

--- AlphaBeta Chess/GameBoard.h
+++ AlphaBeta Chess/GameBoard.h
@@ -22,6 +22,17 @@ public:
 	int blackTotal = 0;
 	int whiteTotal = 0;
 
+	GameBoard(const GameBoard& old_obj) {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				pieces[i][j] = old_obj.pieces[i][j];
+				value[i][j] = old_obj.value[i][j];
+			}
+		}
+		blackTotal = old_obj.blackTotal;
+		whiteTotal = old_obj.whiteTotal;
+	}
+
 	GameBoard();
 	~GameBoard();
 private:
